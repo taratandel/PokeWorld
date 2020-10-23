5fb373210ad5d2f4b24dd2ac8ee779f92cdae5df
Tara <tara.tandel@icloud.com>
2020-10-23 10:44:33 +0200

    Initial Commit

---
+++ BuddyBankSelection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BuddyBankSelection
+//
+//  Created by Tara Tandel on 23/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
