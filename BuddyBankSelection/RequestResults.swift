ae63a7745518d06779d38b193a96d6220d315299
Tara <tara.tandel@icloud.com>
2020-10-23 17:47:26 +0200

    Create Model for networking layer

---
+++ BuddyBankSelection/RequestResults.swift
@@ -0,0 +1,20 @@
+//
+//  RequestResults.swift
+//  BuddyBankSelection
+//
+//  Created by Tara Tandel on 23/10/2020.
+//
+
+import Foundation
+
+struct RequestResults: Codable {
+    var next: String?
+    var previous: String?
+    var results: [PokemonResults]
+}
+
+struct PokemonResults: Codable {
+    var name: String?
+    var url: String?
+}
+
