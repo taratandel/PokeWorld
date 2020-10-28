47dfa7034fc6429d4686176b3a3b38ab55c11484
Tara <tara.tandel@icloud.com>
2020-10-28 22:20:06 +0100

    Add alert controll for the view
    I added some protocls and also alert control for the result of the request because I needed to persenter to be independent for testing reasons

--- BuddyBankSelection/PokemonListPresenter.swift
+++ BuddyBankSelection/PokemonListPresenter.swift
@@ -6,19 +6,26 @@
 //
 
 import Foundation
+import UIKit
+
 class PokemonListPresenter:RequestResponseDelegate {
     
     var reqHandler: ListRequestHandlerDelegate?
+    var view: ListPresenterDelegate?
+    
     var url = EndPoints.serverFirstList
-    func reqForTheList() {
+    private func reqForTheList() {
         if url == "" {return}
         reqHandler?.requesForTheList(url: url)
     }
     
     func reqIsComplete(results: ListRequest) {
         if results.next == nil {
-            var alertText = "It's finished sorry :("
+            let alertText = "It's finished sorry :("
             url = ""
+            let actions = [UIAlertAction(title: "Cancel", style: .cancel, handler: { (action) in
+            })]
+            view?.showAlert(title: "WoW you made it", message: alertText, actions: actions)
             //TODO: show alert that it's finished
         }
         else {
@@ -29,21 +36,43 @@ class PokemonListPresenter:RequestResponseDelegate {
     
     func reqFailed(error: Error) {
         var alertText = ""
-       
+        var actions = [UIAlertAction()]
+        var title = ""
         switch error {
         case RequestErrorType.noInternet:
         //TODO: ask To retry
             alertText = "Please check your internet and try again"
-            
+            actions = [UIAlertAction(title: "Retry", style: .default, handler: { [weak self](action) in
+                self?.reqForTheList()
+            }), UIAlertAction(title: "Cancel", style: .cancel, handler: { (action) in
+            })
+            ]
+            title = "No Internet"
         case RequestErrorType.badRequest:
             alertText = "There is something wrong in our side sorry"
+            actions = [UIAlertAction(title: "Cancel", style: .cancel, handler: { (action) in
+            })]
+            title = "Bad Request"
         case RequestErrorType.serverError:
-            alertText = "Sever is not working :("
+            alertText = "Sever is not working right now :("
+            actions = [UIAlertAction(title: "Retry", style: .default, handler: { [weak self](action) in
+                self?.reqForTheList()
+            }), UIAlertAction(title: "Cancel", style: .cancel, handler: { (action) in
+            })
+            ]
+            title = "Server Error"
         case RequestErrorType.serializationError:
             alertText = "Something's changed on the server!"
+            actions = [UIAlertAction(title: "Cancel", style: .cancel, handler: { (action) in
+            })]
+            title = "Ooops!"
         default:
+            title = "Ooops!"
             alertText = error.localizedDescription
+            actions = [UIAlertAction(title: "Cancel", style: .cancel, handler: { (action) in
+            })]
         }
+        view?.showAlert(title: title, message: alertText, actions: actions)
     }
     
 }
--- BuddyBankSelection/Protocols.swift
+++ BuddyBankSelection/Protocols.swift
@@ -6,12 +6,18 @@
 //
 
 import Foundation
-
+import UIKit
+// handler -> networking
 protocol RequestResponseDelegate: class {
     func reqIsComplete(results: ListRequest)
     func reqFailed(error: Error)
 }
-
+// presenter -> handler
 protocol ListRequestHandlerDelegate: class {
     func requesForTheList(url: String)
+}
+// presenter -> view
+protocol ListPresenterDelegate: class {
+    func reloadPage (nameList: [String])
+    func showAlert (title: String, message: String, actions: [UIAlertAction])
 }
