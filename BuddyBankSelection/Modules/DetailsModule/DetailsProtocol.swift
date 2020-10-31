832ad1350f3f1022f3cbe9536d5f4ae11a6cee88
Tara <tara.tandel@icloud.com>
2020-10-31 15:41:10 +0100

    Create empty files for presenter and needed protocol

---
+++ BuddyBankSelection/Modules/DetailsModule/DetailsProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  DetailsProtocol.swift
+//  BuddyBankSelection
+//
+//  Created by Tara Tandel on 31/10/2020.
+//
+
+import Foundation
---
+++ BuddyBankSelection/Modules/DetailsModule/PokemonDetailsPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  PokemonDetailsPresenter.swift
+//  BuddyBankSelection
+//
+//  Created by Tara Tandel on 31/10/2020.
+//
+
+import Foundation
